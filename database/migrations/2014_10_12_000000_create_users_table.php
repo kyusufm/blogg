843eef133a8d0c3e9f835f4bf16fcdc676f79745
Kyusuf Muhammad <kyusufm@gmail.com>
2015-11-05 17:29:57 +0700

    setup database and migration-script

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name',144);
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->enum('role',['admin','author','subscriber'])->default('author');
             $table->rememberToken();
             $table->timestamps();
         });
